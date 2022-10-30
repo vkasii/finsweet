2b635cd4e0fa1508369ecbdfa8d228249474499d
vkasii <volodymyrkasii@gmail.com>
2022-10-30 10:02:47 +0200

    Added responsive section banner and about us

--- js/script.js
+++ js/script.js
@@ -28,8 +28,13 @@
 
 
 let toggleMenu = document.querySelector('.header__menu-toggle');
-let headerNav = document.querySelector('.header__navigation')
+let headerNav = document.querySelector('.header__navigation');
+let body = document.querySelector('body');
+let main = document.querySelector('.main');
 toggleMenu.addEventListener('click', function() {
    toggleMenu.classList.toggle('active');
    headerNav.classList.toggle('active');
+   body.classList.toggle('lock');
+   main.classList.toggle('hide');
+   
 })
